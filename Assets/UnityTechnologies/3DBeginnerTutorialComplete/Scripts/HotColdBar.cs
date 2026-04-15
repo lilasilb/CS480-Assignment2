e67bec10304d73e3bb784fd34b093b9974b19339
lilasilb <silbermanlila@gmail.com>
2026-04-14 17:52:47 -0700

    Add Hot/Cold text

--- Assets/UnityTechnologies/3DBeginnerTutorialComplete/Scripts/HotColdBar.cs
+++ Assets/UnityTechnologies/3DBeginnerTutorialComplete/Scripts/HotColdBar.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class HotColdBar : MonoBehaviour
 {
     public Transform player;
     public Transform target;
     public Image fillImage;
+    public TMP_Text tempText;
 
     public float maxDistance = 25f;
     public float minDistance = 0.5f;
@@ -19,6 +21,7 @@ public class HotColdBar : MonoBehaviour
         player = GameObject.Find("JohnLemon").transform;
         target = GameObject.Find("EndLocation").transform;
         fillImage = GameObject.Find("Background").GetComponentsInChildren<Image>()[1];
+        tempText = GameObject.Find("TemperatureText").GetComponent<TMP_Text>();
     }
 
     void Update()
@@ -26,11 +29,19 @@ public class HotColdBar : MonoBehaviour
         if (player == null || target == null || fillImage == null) return;
         float distance = Vector3.Distance(player.position, target.position);
         float t = Mathf.Clamp01(Mathf.InverseLerp(maxDistance, minDistance, distance));
-        fillImage.color = Color.Lerp(coldColor, hotColor, t);
-        if (t > 0.8f)
-        {
-            float pulse = (Mathf.Sin(Time.time * 8f) + 1f) / 2f; // oscillates 0–1
-            fillImage.color = Color.Lerp(hotColor, Color.white, pulse * 0.3f);
+        if (t < 0.2f) {
+            tempText.text = "cold";
         }
+        else if (t < 0.6f)
+        {
+            tempText.text = "Warm...";
+        }
+        else
+        {
+            tempText.text = "HOT!!!";
+        }
+        Color color = Color.Lerp(coldColor, hotColor, t);
+        float pulse = (Mathf.Sin(Time.time * 8f) + 1f) / 2f; // oscillates 0–1
+        fillImage.color = Color.Lerp(color, Color.white, pulse * 0.3f);
     }
 }
